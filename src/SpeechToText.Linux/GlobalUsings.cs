ec2ec25e73f272dee16072209d7556592e653753
Jirka <tuma.rsrobot@gmail.com>
2025-12-21 08:34:06 +0100

    Clear repository - project moved to GestureEvolution

--- src/SpeechToText.Linux/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Re-export Core types for backward compatibility
-// Existing code can continue using Olbrasoft.SpeechToText namespace
-
-// Core interfaces and models
-global using Olbrasoft.SpeechToText.Core.Interfaces;
-global using Olbrasoft.SpeechToText.Core.Models;
--- src/SpeechToText.Service/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-// Core interfaces and models
-global using Olbrasoft.SpeechToText.Core.Interfaces;
-global using Olbrasoft.SpeechToText.Core.Models;
